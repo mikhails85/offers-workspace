985c990a4ea47aa085d6f8470d88ab95db91b2ca
Mikhail Sapunov <mis@ciklum.com>
2018-08-01 14:32:45 +0300

    Add selector view

---
+++ src/WebClient/src/components/controls/Selector.vue
@@ -0,0 +1,60 @@
+<template>
+    <b-row>
+        <b-col md="5">  
+            <div>Options</div>          
+            <b-form-select v-model="toAdd" :options="getOptions()" :value-field="fields.value" :text-field="fields.text" class="mb-12 h-100" :select-size="4">
+            </b-form-select>    
+        </b-col>
+        <b-col md="2" class="pt-4 text-center">
+             <b-button class="mb-1" @click.stop=add($event.target)><i class="icon-arrow-right icons mx-2"></i></b-button>             
+             <b-button @click.stop=remove($event.target)><i class="icon-arrow-left icons mx-2"></i></b-button>
+        </b-col>
+        <b-col md="5">            
+            <div>Selected</div>          
+            <b-form-select v-model="toRemove" :options="selected" :value-field="fields.value" :text-field="fields.text" class="mb-12 h-100" :select-size="4">
+            </b-form-select>        
+        </b-col>
+    </b-row>    
+</template>
+<script>
+export default {
+  name: "v-selector",
+  props: ["model", "modelOptions", "valueKey", "textKey"],
+  data() {
+    return {
+      toAdd: null,
+      toRemove: null,
+      selected: this.model,
+      options: this.modelOptions,
+      fields: {
+        value: this.valueKey,
+        text: this.textKey
+      }
+    };
+  },
+  methods: {
+    getOptions() {
+      return this.options.filter(
+        x =>
+          !this.selected.find(
+            s => s[this.fields.value] === x[this.fields.value]
+          )
+      );
+    },
+    add(e) {
+      let addObj = this.options.find(x => x[this.fields.value] === this.toAdd);
+      if (addObj) {
+        this.selected.push(addObj);
+      }
+    },
+    remove(e) {
+      let removeObj = this.selected.find(
+        x => x[this.fields.value] === this.toRemove
+      );
+      if (removeObj) {
+        this.selected.splice(this.selected.indexOf(removeObj), 1);
+      }
+    }
+  }
+};
+</script>
--- src/WebClient/src/components/pages/OfferDetails.vue
+++ src/WebClient/src/components/pages/OfferDetails.vue
@@ -1,15 +1,78 @@
 <template>
-    <b-row>
-        <b-col>
-            //TODO: {{Id}}
-        </b-col>
-    </b-row>
+    <div class="animated fadeIn">
+        <div class="card">
+            <div class="card-header">
+                #{{offer.id}}:{{offer.name}}
+            </div>
+            <div class="card-body">
+                <b-row>
+                    <b-col md="6">
+                        <v-form :frm-model="offer" :frm-fields="formFields"></v-form>
+                    </b-col>
+                    <b-col md="6">
+                        <div class="mb-1">Skills:</div>
+                        <v-selector :model="offer.requaredSkills" :model-options="skills" value-key="id" text-key="name"></v-selector>
+                    </b-col>
+                </b-row>
+                <b-row>
+                    <b-col md="12">
+                        <b-button size="lg" variant="primary" @click.stop="updateOffer" class="mr-1">Save changes</b-button>
+                    </b-col>
+                </b-row>
+            </div>
+        </div>
+    </div>
 </template>
 <script>
+// import axios from "axios";
+import selector from "../controls/Selector.vue";
+import form from "../controls/Form.vue";
 export default {
   name: "OfferDetails",
+  components: {
+    "v-selector": selector,
+    "v-form": form
+  },
   data() {
-    return { Id: this.$route.params.id };
+    return {
+      id: this.$route.params.id,
+      offer: null,
+      skills: null,
+      formFields: [
+        {
+          key: "name",
+          id: "OfferName",
+          type: "text",
+          label: "Offer",
+          placeholder: "Enter offer name"
+        },
+        {
+          key: "description",
+          id: "DescriptionName",
+          type: "lng-text",
+          label: "Description",
+          placeholder: "Enter offer description"
+        }
+      ]
+    };
+  },
+  methods: {
+    updateOffer() {
+      alert(JSON.stringify(this.offer));
+    }
+  },
+  mounted() {
+    this.offer = {
+      id: this.id,
+      name: "TP",
+      description: "Test Project",
+      requaredSkills: []
+    };
+    this.skills = [
+      { id: 1, name: "Skill 1" },
+      { id: 2, name: "Skill 2" },
+      { id: 3, name: "Skill 3" }
+    ];
   }
 };
 </script>
