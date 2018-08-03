eb2521c5d1b1476d4606b7c0897e48f8d7ca29f7
Mikhail <mikhails85@outlook.com>
2018-08-03 10:58:32 +0000

    fixing

--- src/WebClient/src/components/pages/Dashboard.vue
+++ src/WebClient/src/components/pages/Dashboard.vue
@@ -4,20 +4,20 @@
             <b-col md="6">
                 <b-card class="text-white bg-primary">
                     <div class="h1 text-muted text-right mb-4">
-                        <i class="icon-briefcase"></i>
+                        Offers <i class="icon-briefcase"></i>
                     </div>
                     <div class="h4 mb-0">{{model.totalOffers}}</div>
-                    <small class="text-muted text-uppercase font-weight-bold">Offers</small>
+                    <small class="text-muted text-uppercase font-weight-bold">Offers / Employees</small>
                     <b-progress height={} class="progress-white progress-xs mt-3" :value="getOffersPercentage()"/>
                 </b-card>
             </b-col>            
             <b-col md="6">
                 <b-card class="text-white bg-info">
                     <div class="h1 text-muted text-right mb-4">
-                        <i class="icon-user"></i>
+                       Employees <i class="icon-user"></i>
                     </div>
                     <div class="h4 mb-0">{{model.totalEmployees}}</div>
-                    <small class="text-muted text-uppercase font-weight-bold">Employees</small>
+                    <small class="text-muted text-uppercase font-weight-bold">Employees / Offers</small>
                     <b-progress height={} class="progress-white progress-xs mt-3" :value="getEmployeesPercentage()"/>
                 </b-card>
             </b-col>
@@ -25,32 +25,35 @@
     </div>
 </template>
 <script>
+import axios from "axios";
 export default {
-    name:"Dashboard",
-    data(){
-        return {
-            model:null
-        }
+  name: "Dashboard",
+  data() {
+    return {
+      model: null
+    };
+  },
+  methods: {
+    getOffersPercentage() {
+      let total = this.model.totalEmployees + this.model.totalOffers;
+      return Math.round((this.model.totalOffers / total) * 100);
     },
-    methods:{
-       getOffersPercentage(){
-           let total = this.model.totalEmployees+this.model.totalOffers
-           return Math.round((this.model.totalOffers/total)*100)
-       },
-       getEmployeesPercentage(){
-           let total = this.model.totalEmployees+this.model.totalOffers
-           return Math.round((this.model.totalEmployees/total)*100)
-       } 
-    }, 
-    mounted() {
-        let self = this;
-        axios
-            .get("http://es-workspace-mikhails85.c9users.io:8081/api/statistic/totaldocs")
-            .then(r => {
-                if (r.data.success) {
-                    self.model=r.data.value;
-                }
-            });
+    getEmployeesPercentage() {
+      let total = this.model.totalEmployees + this.model.totalOffers;
+      return Math.round((this.model.totalEmployees / total) * 100);
     }
-}
+  },
+  mounted() {
+    let self = this;
+    axios
+      .get(
+        "http://es-workspace-mikhails85.c9users.io:8081/api/statistic/totaldocs"
+      )
+      .then(r => {
+        if (r.data.success) {
+          self.model = r.data.value;
+        }
+      });
+  }
+};
 </script>
